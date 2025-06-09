93222eafce8ec32603508ca4d39e7c8d7a72427d
Ravi Rajani <ravi.inajar@gmail.com>
2025-06-09 14:21:33 +0200

    initialise gem

---
+++ test/asciidoctor/test_html.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Asciidoctor
+  class TestHtml < Minitest::Test
+    def test_that_it_has_a_version_number
+      refute_nil ::Asciidoctor::Html::VERSION
+    end
+
+    # def test_it_does_something_useful
+    #   assert false
+    # end
+  end
+end
