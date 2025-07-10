b2a82b28baf0f06b8107ababa7581ee2934c2786
Ravi Rajani <ravi.inajar@gmail.com>
2025-07-10 11:37:17 +0200

    better book tests

--- test/asciidoctor/html/test_book.rb
+++ test/asciidoctor/html/test_book.rb
@@ -11,6 +11,32 @@ module Asciidoctor
 
       APPENDICES = %w[appendix-a.adoc appendix-b.adoc].map { |f| "#{__dir__}/_book/#{f}" }.freeze
 
+      # Shared tests
+      module Common
+        def test_first_refs_initialized
+          assert_equal "Welcome", @book.refs.dig("index", "chapref")
+          assert_equal "A Section", @book.refs.dig("index", "welcome-section")
+          assert_equal "Theorem 0.1", @book.refs.dig("index", "thm-welcome")
+          assert_equal "Figure 0.1", @book.refs.dig("index", "img-cat")
+        end
+
+        def test_second_refs_initialized
+          assert_equal "Lecture 1", @book.refs.dig("01-introduction", "chapref")
+          assert_equal "Theorem 1.1", @book.refs.dig("01-introduction", "thm-intro")
+          assert_equal "Other Section", @book.refs.dig("01-introduction", "sec-other")
+          assert_equal "Lemma 1.2", @book.refs.dig("01-introduction", "lem-important")
+          assert_equal "A List", @book.refs.dig("01-introduction", "simple-list")
+          assert_equal "2(a)", @book.refs.dig("01-introduction", "simple-list-item")
+        end
+
+        def test_first_templates_initialized
+          assert_equal "", @book.templates["index"]&.chapnum
+          assert_equal "Welcome", @book.templates["index"]&.chaptitle
+          assert_equal "1", @book.templates["01-introduction"]&.chapnum
+          assert_equal "Book&#8217;s Introduction", @book.templates["01-introduction"]&.chaptitle
+        end
+      end
+
       class TestBookNoAppendix < Minitest::Test
         def setup
           @book = Book.new(
@@ -19,31 +45,13 @@ module Asciidoctor
             author: "R. Rajani",
             date: "7/7/2025"
           )
-          @docs = @book.read(CHAPTERS, [])
+          @book.read(CHAPTERS, [])
         end
 
-        def test_first_refs_initialized
-          assert_equal "Welcome", @book.refs["index"]["chapref"]
-          assert_equal "A Section", @book.refs["index"]["welcome-section"]
-        end
-
-        def test_second_refs_initialized
-          assert_equal "Lecture 1", @book.refs["01-introduction"]["chapref"]
-          assert_equal "Theorem 1.1", @book.refs["01-introduction"]["thm-intro"]
-          assert_equal "Other Section", @book.refs["01-introduction"]["sec-other"]
-          assert_equal "Lemma 1.2", @book.refs["01-introduction"]["lem-important"]
-          assert_equal "A List", @book.refs["01-introduction"]["simple-list"]
-          assert_equal "2(a)", @book.refs["01-introduction"]["simple-list-item"]
-        end
+        include Common
 
         def test_third_refs_initialized
           assert_nil @book.refs["appendix-a"]
-        end
-
-        def test_docs_initialized
-          @docs.each do |key, value|
-            assert_equal value, File.read("#{__dir__}/_book/#{key}.html")
-          end
         end
       end
 
@@ -55,34 +63,20 @@ module Asciidoctor
             author: "R. Rajani",
             date: "7/7/2025"
           )
-          @docs = @book.read(CHAPTERS, [APPENDICES.first])
+          @book.read(CHAPTERS, [APPENDICES.first])
         end
 
-        def test_first_refs_initialized
-          assert_equal "Welcome", @book.refs["index"]["chapref"]
-          assert_equal "A Section", @book.refs["index"]["welcome-section"]
-          assert_equal "Theorem 0.1", @book.refs["index"]["thm-welcome"]
-          assert_equal "Figure 0.1", @book.refs["index"]["img-cat"]
-        end
-
-        def test_second_refs_initialized
-          assert_equal "Lecture 1", @book.refs["01-introduction"]["chapref"]
-          assert_equal "Theorem 1.1", @book.refs["01-introduction"]["thm-intro"]
-          assert_equal "Other Section", @book.refs["01-introduction"]["sec-other"]
-          assert_equal "Lemma 1.2", @book.refs["01-introduction"]["lem-important"]
-          assert_equal "A List", @book.refs["01-introduction"]["simple-list"]
-          assert_equal "2(a)", @book.refs["01-introduction"]["simple-list-item"]
-        end
+        include Common
 
         def test_third_refs_initialized
-          assert_equal "Appendix", @book.refs["appendix-a"]["chapref"]
-          assert_equal "Example A.1", @book.refs["appendix-a"]["ex-appa"]
+          assert_equal "Appendix", @book.refs.dig("appendix-a", "chapref")
+          assert_equal "Example A.1", @book.refs.dig("appendix-a", "ex-appa")
         end
 
-        def test_docs_initialized
-          @docs.each do |key, value|
-            assert_equal value, File.read("#{__dir__}/_book/#{key}_2.html")
-          end
+        def test_second_templates_initialized
+          assert_equal "", @book.templates["appendix-a"]&.chapnum
+          assert_equal %(<span class="title-prefix">Appendix</span>Linear Algebra Background),
+                       @book.templates["appendix-a"]&.chaptitle
         end
       end
 
@@ -94,36 +88,25 @@ module Asciidoctor
             author: "R. Rajani",
             date: "7/7/2025"
           )
-          @docs = @book.read(CHAPTERS, APPENDICES)
+          @book.read(CHAPTERS, APPENDICES)
         end
 
-        def test_first_refs_initialized
-          assert_equal "Welcome", @book.refs["index"]["chapref"]
-          assert_equal "A Section", @book.refs["index"]["welcome-section"]
-          assert_equal "Theorem 0.1", @book.refs["index"]["thm-welcome"]
-          assert_equal "Figure 0.1", @book.refs["index"]["img-cat"]
-        end
-
-        def test_second_refs_initialized
-          assert_equal "Lecture 1", @book.refs["01-introduction"]["chapref"]
-          assert_equal "Theorem 1.1", @book.refs["01-introduction"]["thm-intro"]
-          assert_equal "Other Section", @book.refs["01-introduction"]["sec-other"]
-          assert_equal "Lemma 1.2", @book.refs["01-introduction"]["lem-important"]
-          assert_equal "A List", @book.refs["01-introduction"]["simple-list"]
-          assert_equal "2(a)", @book.refs["01-introduction"]["simple-list-item"]
-        end
+        include Common
 
         def test_third_refs_initialized
-          assert_equal "Appendix A", @book.refs["appendix-a"]["chapref"]
-          assert_equal "Appendix B", @book.refs["appendix-b"]["chapref"]
-          assert_equal "Example A.1", @book.refs["appendix-a"]["ex-appa"]
-          assert_equal "Other Results", @book.refs["appendix-b"]["sec-other"]
+          assert_equal "Appendix A", @book.refs.dig("appendix-a", "chapref")
+          assert_equal "Appendix B", @book.refs.dig("appendix-b", "chapref")
+          assert_equal "Example A.1", @book.refs.dig("appendix-a", "ex-appa")
+          assert_equal "Other Results", @book.refs.dig("appendix-b", "sec-other")
         end
 
-        def test_docs_initialized
-          @docs.each do |key, value|
-            assert_equal value, File.read("#{__dir__}/_book/#{key}_3.html")
-          end
+        def test_second_templates_initialized
+          assert_equal "", @book.templates["appendix-a"]&.chapnum
+          assert_equal %(<span class="title-prefix">Appendix A</span>Linear Algebra Background),
+                       @book.templates["appendix-a"]&.chaptitle
+          assert_equal "", @book.templates["appendix-b"]&.chapnum
+          assert_equal %(<span class="title-prefix">Appendix B</span>Analysis Background),
+                       @book.templates["appendix-b"]&.chaptitle
         end
       end
     end
