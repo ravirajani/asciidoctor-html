ddfb6b93ec5d961606a438b60ce08a1db019333c
Ravi Rajani <ravi.inajar@gmail.com>
2026-08-09 17:27:08 +0200

    jupyterlite loading behaviour

--- lib/asciidoctor/html/jupyterlite.rb
+++ lib/asciidoctor/html/jupyterlite.rb
@@ -4,6 +4,8 @@ module Asciidoctor
   module Html
     # Constants for the jupyterlite library
     module Jupyterlite
+      @@id = 0
+
       PATH = "jupyterlite"
       REPL_PATH = "repl/index.html"
 
@@ -37,13 +39,36 @@ module Asciidoctor
       end
 
       def self.html(node)
+        @@id += 1
+        spinner_id = "jupyter-cell-spinner-#{@@id}"
+        cell_id = "jupyter-cell-frame-#{@@id}"
+        height_attr = %( style="height: #{node.attr("height")}px;") if node.attr?("height")
         <<~HTML
-          <iframe
-            src="#{PATH}/#{repl_path_with_options node}"
-            width="100%"
-            height=#{node.attr("height", "100%")}
-            loading="lazy"
-          ></iframe>
+        <div id="#{cell_id}"#{height_attr}>
+          <div id="#{spinner_id}" class="d-flex justify-content-center align-items-center"#{height_attr}>
+            <div class="spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </div>
+        <script>
+          (function(){
+            const spinner = document.getElementById('#{spinner_id}');
+            const cell = document.getElementById('#{cell_id}');
+            addEventListener('load', () => {
+              const frame = document.createElement('iframe');
+              frame.classList.add('hidden');
+              frame.src = '#{PATH}/#{repl_path_with_options node}';
+              frame.width = '100%';
+              frame.height = '100%';
+              cell.appendChild(frame);
+              frame.addEventListener('load', () => {
+                spinner.classList.add('hidden');
+                frame.classList.remove('hidden');
+              });
+            });
+          })();
+        </script>
         HTML
       end
     end
